fad3a9b3c3fc325f80f49689a124e2c7c0383746
nunnapatphuttha <nunnapatphutthatipakorn@gmail.com>
2022-03-18 21:05:45 +0700

    layout

--- src/page/nav/nav.tsx
+++ src/page/nav/nav.tsx
@@ -1,23 +1,24 @@
-import { Link } from "react-router-dom"
-import "./nav.css"
-import NavData from "../../data/navdata"
+import { Link } from "react-router-dom";
+import "./nav.css";
+import NavData from "../../data/navdata";
 
-const NavComponent =()=> {
-    return(
-        <div>
-            <nav className="nav-menu">
-                <ul className="nav-menu-item">
-                    {NavData.map((nav,index)=>{
-                        return(
-                        <li className="nav-text" key={index}>
-                            <Link to={nav.path}>{nav.icon}<span>{nav.title}</span></Link>
-                        </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-        </div>
-    )
-}
+const NavComponent = () => {
+  return (
+    <div>
+      <nav className="nav-menu">
+        {NavData.map((nav, index) => {
+          return (
+            <li className="nav-text" key={index}>
+              <Link to={nav.path}>
+                <span className="nav-icon">{nav.icon}</span>
+                <span>{nav.title}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </nav>
+    </div>
+  );
+};
 
-export default NavComponent
+export default NavComponent;
