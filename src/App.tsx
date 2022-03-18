f424a6c2c5b95dc6cf586b280517ae7ac4856133
nunnapatphuttha <nunnapatphutthatipakorn@gmail.com>
2022-03-18 19:43:55 +0700

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import ButtonLogin from "./component/button/button-login";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="App">
-      
+      <ButtonLogin />
     </div>
   );
 }
