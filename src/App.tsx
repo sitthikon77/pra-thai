5753cb06919794e0da067aa9f6149569eab77bb5
Sitthikon <sitthikon.work@gmail.com>
2022-03-19 13:36:54 +0700

    create route

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import NavComponent from "./page/nav/nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListPraPage from "./page/list-pra-page/list-pra-page";
 import HomePage from "./page/home-page/home-page";
+import CerPage from "./page/cerificate-page/certificate-page";
+import NewsPage from "./page/news-page/news-page";
+import ContractPage from "./page/contract-page/contract-page";
+import PolicyPage from "./page/policy-page/policy-page";
 
 function App() {
   return (
     <div className="App">
-      <div className="header"></div>
+      <div className="header"><img src="" alt="" /></div>
       <Router>
         <div className="container-fluid">
           <div className="row">
@@ -18,6 +22,10 @@ function App() {
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/list-pra-page" element={<ListPraPage />} />
+                <Route path="/certificate-page" element={<CerPage />} />
+                <Route path="/news-page" element={<NewsPage />} />
+                <Route path="/contract-page" element={<ContractPage />} />
+                <Route path="/policy-page" element={<PolicyPage />} />
               </Routes>
             </div>
             <div className="col">One of three columns</div>
---
+++ src/page/cerificate-page/certificate-page.tsx
@@ -0,0 +1,9 @@
+const CerPage = () => {
+    return (
+        <div>
+            <h1>Certificate Page</h1>
+        </div>
+    )
+}
+
+export default CerPage
