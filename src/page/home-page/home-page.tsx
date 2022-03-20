8257e159a4a3d2d4d1a52e76c0878c41108410af
nunnapatphuttha <nunnapatphutthatipakorn@gmail.com>
2022-03-20 20:03:58 +0700

    add side

--- src/page/home-page/home-page.tsx
+++ src/page/home-page/home-page.tsx
@@ -4,14 +4,11 @@ import StoryData from "../../data/storydata";
 import "./home-page.css";
 import "../../component/story/story.css";
 import AddStoryComponent from "../../component/story/add-story";
-import PostComponent from "./post-component/post-component";
-import Post from "./post-component/post";
 
 const HomePage = () => {
   const [storyData, setStoryData] = useState(StoryData);
-
   return (
-    <div>
+    <div className=".container-fluid	">
       <div className="container" id="home-container">
         <AddStoryComponent />
         {/* STORY */}
@@ -19,15 +16,6 @@ const HomePage = () => {
           return <StoryComponent key={index} {...data} />;
         })}
         {/* STORY */}
-      </div>
-
-      {/* POST */}
-      <div>
-        <PostComponent
-          addPost={function (...args: any[]) {
-            throw new Error("Function not implemented.");
-          }}
-        />
       </div>
     </div>
   );
